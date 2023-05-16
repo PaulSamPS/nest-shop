adbe903902437397275378e0160822bcc377a5ba
PaulSamPS <psamoylenko2@gmail.com>
2023-05-16 14:35:26 +0300

    serve  static

--- src/config/file-upload.ts
+++
@@ -1,3 +0,0 @@
-import { path } from 'app-root-path';
-import { ensureDir, writeFile } from 'fs-extra';
-import { FileElementResponse } from '../files/dto/file-element-response.response';
--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { FilesService } from './files.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { path } from 'app-root-path';
 
 @Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: `${path}/uploads`,
+      serveRoot: '/static',
+    }),
+  ],
   providers: [FilesService],
 })
 export class FilesModule {}
