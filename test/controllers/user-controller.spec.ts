9e9cbb651482bb48c59ab776629fe87065657c5b
PaulSamPS <psamoylenko2@gmail.com>
2023-05-21 08:07:13 +0300

    create user test

---
+++ test/controllers/user-controller.spec.ts
@@ -0,0 +1,56 @@
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+import { databaseConfig, SequelizeConfigService } from '@config';
+import { UsersModule, User } from '@users';
+import * as bcrypt from 'bcrypt';
+import * as request from 'supertest';
+
+describe('users controller', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const testModule: TestingModule = await Test.createTestingModule({
+      imports: [
+        SequelizeModule.forRootAsync({
+          imports: [ConfigModule],
+          useClass: SequelizeConfigService,
+        }),
+        ConfigModule.forRoot({
+          load: [databaseConfig],
+        }),
+        UsersModule,
+      ],
+    }).compile();
+    app = testModule.createNestApplication();
+    await app.init();
+  });
+
+  afterEach(async () => {
+    await User.destroy({ where: { username: 'test' } });
+  });
+
+  it('should create user', async () => {
+    const newUser = {
+      username: 'test',
+      email: 'test@mail.com',
+      password: '12345',
+      phone: '+71234567890',
+    };
+
+    const response = await request(app.getHttpServer())
+      .post('/users/signup')
+      .send(newUser);
+
+    const passwordIsValid = await bcrypt.compare(
+      newUser.password,
+      response.body.password,
+    );
+
+    expect(response.body.username).toBe(newUser.username);
+    expect(passwordIsValid).toBe(true);
+    expect(response.body.email).toBe(newUser.email);
+    expect(response.body.phone).toBe(newUser.phone);
+  });
+});
---
+++ test/providers/users.service.spec.ts
@@ -0,0 +1,56 @@
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+import { databaseConfig, SequelizeConfigService } from '@config';
+import { UsersModule, User, UsersService } from '@users';
+import * as bcrypt from 'bcrypt';
+
+describe('users controller', () => {
+  let app: INestApplication;
+  let usersService: UsersService;
+
+  beforeEach(async () => {
+    const testModule: TestingModule = await Test.createTestingModule({
+      imports: [
+        SequelizeModule.forRootAsync({
+          imports: [ConfigModule],
+          useClass: SequelizeConfigService,
+        }),
+        ConfigModule.forRoot({
+          load: [databaseConfig],
+        }),
+        UsersModule,
+      ],
+    }).compile();
+
+    usersService = testModule.get<UsersService>(UsersService);
+    app = testModule.createNestApplication();
+    await app.init();
+  });
+
+  afterEach(async () => {
+    await User.destroy({ where: { username: 'test' } });
+  });
+
+  it('should create user', async () => {
+    const newUser = {
+      username: 'test',
+      email: 'test@mail.com',
+      password: '12345',
+      phone: '+71234567890',
+    };
+
+    const user = (await usersService.create(newUser)) as User;
+
+    const passwordIsValid = await bcrypt.compare(
+      newUser.password,
+      user.password,
+    );
+
+    expect(user.username).toBe(newUser.username);
+    expect(passwordIsValid).toBe(true);
+    expect(user.email).toBe(newUser.email);
+    expect(user.phone).toBe(newUser.phone);
+  });
+});
